42b72ccb94104bd952f53370139c8dcd75e988b1
Matthew Denaburg <EMAIL>
2019-05-31 11:13:35 -0400

    auto-install Vundle

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -3,5 +3,7 @@ mkdir -p $DOTFILES/caches/vim
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
+  mkdir -p "$DOTFILES/link/.vim/bundle/"
+  git clone 'https://github.com/VundleVim/Vundle.vim/' "$DOTFILES/link/.vim/bundle/Vundle.vim"
   vim +PluginInstall +qall
 fi
