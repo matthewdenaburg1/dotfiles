29a36f431c285853f71a5f32a149d7fef8f83ca8
Matthew Denaburg <EMAIL>
2019-06-02 16:31:29 -0400

    some 3rd party bash completion stuff

---
+++ source/50_completion.sh
@@ -0,0 +1,34 @@
+#!/usr/local/bin/env bash
+
+completions=()
+
+if is_osx; then
+    # brew puts some bash completion stuff in /usr/local/etc/bash_completion.d
+    COMPLETION_LOC='/usr/local/etc/bash_completion.d'
+
+    completions+=(
+        $COMPLETION_LOC/git-completion.bash
+        $COMPLETION_LOC/brew
+    )
+    # googler
+    if [[ "$(type -P googler)" ]]; then
+        completions+=($COMPLETION_LOC/googler-completion.bash)
+    fi
+    # todo.sh
+    if [[ "$(type -P todo.sh)" ]]; then
+        completions+=($COMPLETION_LOC/todo_completion)
+    fi
+    # bash completion
+    if is_osx && [[ -d '/usr/local/share/bash-completion/bash_completion' ]]; then
+        completions+=(/usr/local/share/bash-completion/bash_completion)
+    fi
+    unset COMPLETION_LOC
+fi
+
+if (( ${#completions[@]} > 0 )) ; then
+    for loc in "${completions[@]}"; do
+        [[ -e "$loc" ]] && source $loc
+    done
+    unset loc
+fi
+unset completions
