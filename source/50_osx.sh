f88b970d52f993b9d5f81fedd0c20ba77cd7979f
Matthew Denaburg <matthew.denaburg1@gmail.com>
2019-06-05 15:39:41 -0400

    update ss alias so it actually works

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -14,7 +14,7 @@ alias c="tr -d '\n' | pbcopy"
 [[ "$(type -P lesspipe.sh)" ]] && eval "$(lesspipe.sh)"
 
 # Start ScreenSaver. This will lock the screen if locking is enabled.
-alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+alias ss="open -a ScreenSaverEngine.app"
 
 function __hidden_files_help() {
     cat <<EOF
