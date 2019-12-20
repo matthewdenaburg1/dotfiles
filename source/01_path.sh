2fa526be0e24b1dbe66408709ec56353b3b9a16e
Matthew Denaburg <matthew.denaburg1@gmail.com>
2019-12-20 10:11:47 -0500

    reverting changes

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,24 +1,11 @@
-ORACLE_HOME=/opt/oracle/instantclient_10_2/
-export ORACLE_HOME
-
 paths=(
   ~/.local/bin
   $DOTFILES/bin
   /usr/local/opt/coreutils/libexec/gnubin
-  $ORACLE_HOME/bin
 )
-
-libs=(
-  $ORACLE_HOME/lib
-)
-
-export LD_LIBRARY_PATH
-for l in "${libs[@]}"; do
-	[[ -d "$l" ]] && LD_LIBRARY_PATH="$l:$(lib_path_remove "$l")"
-done
 
 export PATH
 for p in "${paths[@]}"; do
   [[ -d "$p" ]] && PATH="$p:$(path_remove "$p")"
 done
-unset l libs p paths
+unset p paths
