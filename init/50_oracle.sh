fab2e24f81364744504eeab7c45b5c26ce159367
Matthew Denaburg <matthew.denaburg1@gmail.com>
2020-01-20 08:50:09 -0500

    remove stuff that is not working

--- init/50_oracle.sh
+++ init/50_oracle.sh
@@ -1,15 +1,8 @@
 ora_url='https://www.oracle.com/database/technologies/instant-client/linux-x86-64-downloads.html'
 
 cat <<-EOF
-Also install oracle connector:
+Also install oracle connector if needed.
 ${ora_url}
 - Instant Client Basic for version
 - sqlplus-10.2.0.5.0-linux-x64.zip
-
-if needed
 EOF
-
-mkdir -p /opt/oracle/instantclient_10_2
-cd /opt/oracle/instantclient_10_2
-mkdir -p bin network/admin sqlplus/admin sqlplus/mesg
-cd -
