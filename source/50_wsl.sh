ce27ccdbee06611fab297774344f23bee83f3b7c
Matthew Denaburg <matthew.denaburg1@gmail.com>
2020-02-01 16:13:34 -0500

    use is_wsl

--- source/50_wsl.sh
+++ source/50_wsl.sh
@@ -1,8 +1,10 @@
 ### Windows Subsystem for Linux
 
-is_ubuntu || return 1
+is_wsl || return 1
 
 # allow browser links to be opened in default browser
 if [[ "$(which wslview)" ]]; then
 	export BROWSER="wslview"
 fi
+
+export DISPLAY=:0
