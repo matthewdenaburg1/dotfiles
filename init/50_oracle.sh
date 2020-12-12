1890065031e0f608108bd6a52a774b0da18cf40e
Matthew Denaburg <EMAIL>
2020-12-12 17:07:29 -0500

    remove useless init file

--- init/50_oracle.sh
+++
@@ -1,8 +0,0 @@
-ora_url='https://www.oracle.com/database/technologies/instant-client/linux-x86-64-downloads.html'
-
-cat <<-EOF
-Also install oracle connector if needed.
-${ora_url}
-- Instant Client Basic for version
-- sqlplus-10.2.0.5.0-linux-x64.zip
-EOF
