e61d56d5653b1ae9f2ee11ffa69fdf5efa622f69
Matthew Denaburg <matthew.denaburg1@gmail.com>
2020-12-18 09:14:12 -0500

    remove unnecessary oracle stuff

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,24 +1,11 @@
-ORACLE_HOME=/opt/oracle/instantclient_10_2
-[[ -d $ORACLE_HOME ]] && export ORACLE_HOME
-
 paths=(
     ~/.local/bin
     $DOTFILES/bin
     /usr/local/opt/coreutils/libexec/gnubin
-    $ORACLE_HOME/bin
 )
-
-libs=(
-    $ORACLE_HOME/lib
-)
-
-export LD_LIBRARY_PATH
-for lib in "${libs[@]}"; do
-    [[ -d "$lib" ]] && LD_LIBRARY_PATH="$lib:$(lib_path_remove "$lib")"
-done
 
 export PATH
 for pth in "${paths[@]}"; do
     [[ -d "$pth" ]] && PATH="$pth:$(path_remove "$pth")"
 done
-unset lib libs pth paths
+unset pth paths
